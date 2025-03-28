856ad168018bf0780c5620846e14af93377e33cb
Maher Ahmad Awada <113375570+MaherAwada@users.noreply.github.com>
2025-03-28 18:33:07 -0300

    Delete back-end directory

--- back-end/src/main/java/br/com/iaragames/cadastro/repository/JogoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.iaragames.cadastro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.iaragames.cadastro.model.Jogo;
-
-public interface JogoRepository extends JpaRepository <Jogo, Long> {
-    
-}
--- back-end/src/main/java/br/com/iaragames/cadastro/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.iaragames.cadastro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.iaragames.cadastro.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
